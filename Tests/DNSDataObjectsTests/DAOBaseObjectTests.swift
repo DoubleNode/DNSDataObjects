d43d2589a2f41e2b625768096d86db56293053db
Darren Ehlers <me@darrenehlers.com>
2020-06-24 14:13:57 -0500

    Updated comment headers

--- Tests/DNSDataObjectsTests/DAOBaseObjectTests.swift
+++ Tests/DNSDataObjectsTests/DAOBaseObjectTests.swift
@@ -1,3 +1,11 @@
+//
+//  DNSBaseObjectTests.swift
+//  DoubleNode Swift Framework (DNSFramework) - DNSDataObjectsTests
+//
+//  Created by Darren Ehlers.
+//  Copyright © 2020 - 2016 DoubleNode.com. All rights reserved.
+//
+
 import XCTest
 @testable import DNSDataObjects
 
