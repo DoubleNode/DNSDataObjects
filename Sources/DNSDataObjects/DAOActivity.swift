5d3fdf2d4687d90dde9114194dc7c1e0d9a61f23
Darren Ehlers <me@darrenehlers.com>
2021-02-01 15:10:32 -0600

    Update ActivityType property from type to baseType

--- Sources/DNSDataObjects/DAOActivity.swift
+++ Sources/DNSDataObjects/DAOActivity.swift
@@ -9,19 +9,19 @@
 import Foundation
 
 open class DAOActivity: DAOBaseObject {
+    public var baseType: DAOActivityType?
     public var code: String
     public var name: String
-    public var type: DAOActivityType?
     open var beacons: [DAOBeacon] = []
 
     private enum CodingKeys: String, CodingKey {
-        case code, name, type /*, beacons*/
+        case code, name, baseType /*, beacons*/
     }
     required public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         code = try container.decode(String.self, forKey: .code)
         name = try container.decode(String.self, forKey: .name)
-        type = try container.decode(DAOActivityType.self, forKey: .type)
+        baseType = try container.decode(DAOActivityType.self, forKey: .baseType)
 
         // Get superDecoder for superclass and call super.init(from:) with it
         let superDecoder = try container.superDecoder()
@@ -29,6 +29,7 @@ open class DAOActivity: DAOBaseObject {
     }
 
     override public init() {
+        self.baseType = nil
         self.code = ""
         self.name = ""
         self.beacons = []
@@ -37,6 +38,7 @@ open class DAOActivity: DAOBaseObject {
     }
 
     override public init(id: String) {
+        self.baseType = nil
         self.code = ""
         self.name = ""
         self.beacons = []
@@ -45,6 +47,7 @@ open class DAOActivity: DAOBaseObject {
     }
     
     override public init(from dictionary: Dictionary<String, Any?>) {
+        self.baseType = nil
         self.code = ""
         self.name = ""
         self.beacons = []
@@ -55,15 +58,16 @@ open class DAOActivity: DAOBaseObject {
     }
     
     public init(from object: DAOActivity) {
+        self.baseType = object.baseType
         self.code = object.code
         self.name = object.name
-        self.type = object.type
         self.beacons = object.beacons
         
         super.init(from: object)
     }
     
     public init(code: String, name: String) {
+        self.baseType = nil
         self.code = code
         self.name = name
         self.beacons = []
@@ -72,15 +76,16 @@ open class DAOActivity: DAOBaseObject {
     }
     
     open func update(from object: DAOActivity) {
+        self.baseType = object.baseType
         self.code = object.code
         self.name = object.name
-        self.type = object.type
         self.beacons = object.beacons
 
         super.update(from: object)
     }
 
     override open func dao(from dictionary: [String: Any?]) -> DAOActivity {
+        // TODO: Implement baseType import
         self.code = self.string(from: dictionary["code"] as Any?) ?? self.code
         self.name = self.string(from: dictionary["name"] as Any?) ?? self.name
 
@@ -99,6 +104,7 @@ open class DAOActivity: DAOBaseObject {
     override open func dictionary() -> [String: Any?] {
         var retval = super.dictionary()
         retval.merge([
+            "baseType": self.baseType?.code,
             "code": self.code,
             "name": self.name,
             "beacons": self.beacons.map { $0.dictionary() },
