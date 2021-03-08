0b21d4532a42277dd4afd1182df4f277e2305489
Darren Ehlers <me@darrenehlers.com>
2021-03-08 13:18:06 -0600

    Added DAOBeaconDistance

---
+++ Sources/DNSDataObjects/DAOBeaconDistance.swift
@@ -0,0 +1,54 @@
+//
+//  DAOBeaconDistance.swift
+//  DoubleNode Swift Framework (DNSFramework) - DNSDataObjects
+//
+//  Created by Darren Ehlers.
+//  Copyright © 2020 - 2016 DoubleNode.com. All rights reserved.
+//
+
+import Foundation
+
+public enum DAOBeaconDistance: String, CaseIterable, Codable {
+    case unknown
+    case distant
+    case far
+    case nearby
+    case close
+    case immediate
+
+    var code: String {
+        switch self {
+        case .unknown:  return "unk"
+        case .distant:  return "dis"
+        case .far:      return "far"
+        case .nearby:   return "nea"
+        case .close:    return "clo"
+        // .immediate
+        default:        return "imm"
+        }
+    }
+
+    var index: Int {
+        switch self {
+        case .unknown:  return 1
+        case .distant:  return 2
+        case .far:      return 3
+        case .nearby:   return 4
+        case .close:    return 5
+        // .immediate
+        default:        return 6
+        }
+    }
+
+    var signalLevel: Int8 {
+        switch self {
+        case .unknown:  return 5
+        case .distant:  return 20
+        case .far:      return 45
+        case .nearby:   return 70
+        case .close:    return 95
+        // .immediate
+        default:        return 100
+        }
+    }
+}
