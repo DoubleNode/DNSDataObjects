59216ffadc9029fa7e0b874ebd89cc818028256d
Darren Ehlers <me@darrenehlers.com>
2021-08-26 12:49:43 -0500

    Added bookingEndTime/bookingStartTime to DAOActivity object

--- Sources/DNSDataObjects/DAOActivity.swift
+++ Sources/DNSDataObjects/DAOActivity.swift
@@ -10,18 +10,22 @@ import Foundation
 
 open class DAOActivity: DAOBaseObject {
     public var baseType: DAOActivityType?
+    public var bookingEndTime: Date?
+    public var bookingStartTime: Date?
     public var code: String
     public var name: String
     open var beacons: [DAOBeacon] = []
 
     private enum CodingKeys: String, CodingKey {
-        case code, name, baseType /*, beacons*/
+        case baseType, bookingEndTime, bookingStartTime, code, name /*, beacons*/
     }
     required public init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
+        baseType = try container.decode(DAOActivityType.self, forKey: .baseType)
+        bookingEndTime = try container.decode(Date?.self, forKey: .bookingEndTime)
+        bookingStartTime = try container.decode(Date?.self, forKey: .bookingStartTime)
         code = try container.decode(String.self, forKey: .code)
         name = try container.decode(String.self, forKey: .name)
-        baseType = try container.decode(DAOActivityType.self, forKey: .baseType)
         // Get superDecoder for superclass and call super.init(from:) with it
         let superDecoder = try container.superDecoder()
         try super.init(from: superDecoder)
@@ -29,9 +33,11 @@ open class DAOActivity: DAOBaseObject {
     override open func encode(to encoder: Encoder) throws {
         try super.encode(to: encoder)
         var container = encoder.container(keyedBy: CodingKeys.self)
+        if baseType != nil { try container.encode(baseType, forKey: .baseType) }
+        try container.encode(bookingEndTime, forKey: .bookingEndTime)
+        try container.encode(bookingStartTime, forKey: .bookingStartTime)
         try container.encode(code, forKey: .code)
         try container.encode(name, forKey: .name)
-        if baseType != nil { try container.encode(baseType, forKey: .baseType) }
     }
 
     override public init() {
@@ -58,6 +64,8 @@ open class DAOActivity: DAOBaseObject {
     }
     public init(from object: DAOActivity) {
         self.baseType = object.baseType
+        self.bookingEndTime = object.bookingEndTime
+        self.bookingStartTime = object.bookingStartTime
         self.code = object.code
         self.name = object.name
         self.beacons = object.beacons
@@ -72,6 +80,8 @@ open class DAOActivity: DAOBaseObject {
     }
     open func update(from object: DAOActivity) {
         self.baseType = object.baseType
+        self.bookingEndTime = object.bookingEndTime
+        self.bookingStartTime = object.bookingStartTime
         self.code = object.code
         self.name = object.name
         self.beacons = object.beacons
@@ -80,6 +90,8 @@ open class DAOActivity: DAOBaseObject {
 
     override open func dao(from dictionary: [String: Any?]) -> DAOActivity {
         // TODO: Implement baseType import
+        self.bookingEndTime = self.date(from: dictionary["bookingEndTime"] as Any?)
+        self.bookingStartTime = self.date(from: dictionary["bookingStartTime"] as Any?)
         self.code = self.string(from: dictionary["code"] as Any?) ?? self.code
         self.name = self.string(from: dictionary["name"] as Any?) ?? self.name
         var beacons: [DAOBeacon] = []
@@ -95,6 +107,8 @@ open class DAOActivity: DAOBaseObject {
         var retval = super.dictionary()
         retval.merge([
             "baseType": self.baseType?.code,
+            "bookingEndTime": self.bookingEndTime,
+            "bookingStartTime": self.bookingStartTime,
             "code": self.code,
             "name": self.name,
             "beacons": self.beacons.map { $0.dictionary() },
