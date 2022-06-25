76395060e267507cf99974af134de7a75cbf090f
Darren Ehlers <me@darrenehlers.com>
2022-06-24 21:10:31 -0500

    Fixed method access limit

--- Sources/DNSDataObjects/DAOAppAction.swift
+++ Sources/DNSDataObjects/DAOAppAction.swift
@@ -44,7 +44,7 @@ open class DAOAppAction: DAOBaseObject, NSCopying {
         let superDecoder = try container.superDecoder()
         try super.init(from: superDecoder)
     }
-    override public func encode(to encoder: Encoder) throws {
+    override open func encode(to encoder: Encoder) throws {
         try super.encode(to: encoder)
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(actionType, forKey: .actionType)
