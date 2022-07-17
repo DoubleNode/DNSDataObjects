a4c37f64a31f3e4b65a6ec8c901c6be0a7a9bae4
Darren Ehlers <me@darrenehlers.com>
2022-07-17 15:27:30 -0500

    Added CurrentState shortcuts to System and SystemEndPoint data objects

---
+++ Sources/DNSDataObjects/DAOSystem+currentState.swift
@@ -0,0 +1,28 @@
+//
+//  DAOSystem+currentState.swift
+//  DoubleNode Swift Framework (DNSFramework) - DNSDataObjects
+//
+//  Created by Darren Ehlers.
+//  Copyright © 2020 - 2016 DoubleNode.com. All rights reserved.
+//
+
+import DNSCore
+import Foundation
+
+extension DAOSystem {
+    public var failureCodes: [String: DNSSystemStateNumbers] {
+        self.currentState.failureCodes
+    }
+    public var failureRate: DNSSystemStateNumbers {
+        self.currentState.failureRate
+    }
+    public var totalPoints: DNSSystemStateNumbers {
+        self.currentState.totalPoints
+    }
+    public var state: DNSSystemState {
+        self.currentState.state
+    }
+    public var stateOverride: DNSSystemState {
+        self.currentState.stateOverride
+    }
+}
