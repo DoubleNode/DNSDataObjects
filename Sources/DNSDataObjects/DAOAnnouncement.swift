98662d55655c986365232f3500b6358f451f69c0
Darren Ehlers <me@darrenehlers.com>
2022-11-23 17:53:17 -0600

    Added DNSVisibility and distribution field to Announcement and Event DAOs

--- Sources/DNSDataObjects/DAOAnnouncement.swift
+++ Sources/DNSDataObjects/DAOAnnouncement.swift
@@ -31,10 +31,11 @@ open class DAOAnnouncement: DAOBaseObject, DecodingConfigurationProviding, Encod
     // MARK: - Properties -
     private func field(_ from: CodingKeys) -> String { return from.rawValue }
     public enum CodingKeys: String, CodingKey {
-        case body, endTime, startTime, title
+        case body, distribution, endTime, startTime, title
     }
 
     open var body = DNSString()
+    open var distribution = DNSVisibility.everyone
     open var endTime: Date?
     open var startTime = Date()
     open var title = DNSString()
@@ -55,6 +56,7 @@ open class DAOAnnouncement: DAOBaseObject, DecodingConfigurationProviding, Encod
     open func update(from object: DAOAnnouncement) {
         super.update(from: object)
         self.body = object.body
+        self.distribution = object.distribution
         self.endTime = object.endTime
         self.startTime = object.startTime
         self.title = object.title
@@ -68,6 +70,8 @@ open class DAOAnnouncement: DAOBaseObject, DecodingConfigurationProviding, Encod
     override open func dao(from data: DNSDataDictionary) -> DAOAnnouncement {
         _ = super.dao(from: data)
         self.body = self.dnsstring(from: data[field(.body)] as Any?) ?? self.body
+        let distributionData = self.string(from: data[field(.distribution)] as Any?) ?? self.distribution.rawValue
+        self.distribution = DNSVisibility(rawValue: distributionData) ?? .everyone
         self.endTime = self.time(from: data[field(.endTime)] as Any?)
         self.startTime = self.time(from: data[field(.startTime)] as Any?) ?? self.startTime
         self.title = self.dnsstring(from: data[field(.title)] as Any?) ?? self.title
@@ -77,6 +81,7 @@ open class DAOAnnouncement: DAOBaseObject, DecodingConfigurationProviding, Encod
         var retval = super.asDictionary
         retval.merge([
             field(.body): self.body.asDictionary,
+            field(.distribution): self.distribution.rawValue,
             field(.endTime): self.endTime,
             field(.startTime): self.startTime,
             field(.title): self.title.asDictionary,
@@ -107,11 +112,14 @@ open class DAOAnnouncement: DAOBaseObject, DecodingConfigurationProviding, Encod
         endTime = self.time(from: container, forKey: .endTime)
         startTime = self.time(from: container, forKey: .startTime) ?? startTime
         title = self.dnsstring(from: container, forKey: .title) ?? title
+
+        distribution = try container.decodeIfPresent(Swift.type(of: distribution), forKey: .distribution) ?? distribution
     }
     open func encode(to encoder: Encoder, configuration: Config) throws {
         try super.encode(to: encoder, configuration: configuration)
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(body, forKey: .body)
+        try container.encode(distribution, forKey: .distribution)
         try container.encode(endTime, forKey: .endTime)
         try container.encode(startTime, forKey: .startTime)
         try container.encode(title, forKey: .title)
@@ -128,6 +136,7 @@ open class DAOAnnouncement: DAOBaseObject, DecodingConfigurationProviding, Encod
         let lhs = self
         return super.isDiffFrom(rhs) ||
             lhs.body != rhs.body ||
+            lhs.distribution != rhs.distribution ||
             lhs.endTime != rhs.endTime ||
             lhs.startTime != rhs.startTime ||
             lhs.title != rhs.title
---
+++ Sources/DNSDataObjects/SimpleObjects/DNSVisibility.swift
@@ -0,0 +1,16 @@
+//
+//  DNSVisibility.swift
+//  DoubleNode Swift Framework (DNSFramework) - DNSDataObjects
+//
+//  Created by Darren Ehlers.
+//  Copyright © 2022 - 2016 DoubleNode.com. All rights reserved.
+//
+
+import DNSCore
+import UIKit
+
+public enum DNSVisibility: String, CaseIterable, Codable {
+    case adultsOnly
+    case everyone
+    case staffOnly
+}
