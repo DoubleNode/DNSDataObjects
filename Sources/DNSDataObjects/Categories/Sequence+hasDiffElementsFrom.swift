041bebc3f263a3a9c4a6e33874a277246d5ffb7c
Darren Ehlers <me@darrenehlers.com>
2022-12-07 21:53:42 -0600

    Added count to Sequences and tweaked hasDiffElementsFrom method

--- Sources/DNSDataObjects/Categories/Sequence+hasDiffElementsFrom.swift
+++ Sources/DNSDataObjects/Categories/Sequence+hasDiffElementsFrom.swift
@@ -11,8 +11,10 @@ import Foundation
 import KeyedCodable
 
 public extension Sequence where Element: DAOBaseObject {
+    var count: Int { return reduce(0) { acc, row in acc + 1 } }
+
     func hasDiffElementsFrom(_ rhs: [DAOBaseObject]) -> Bool {
-        var anyNonMatches = false
+        var anyNonMatches = (self.count != rhs.count)
         self.forEach { lhsElement in
             let foundMatch = rhs.reduce(false) { $0 || !lhsElement.isDiffFrom($1) }
             if !foundMatch {
