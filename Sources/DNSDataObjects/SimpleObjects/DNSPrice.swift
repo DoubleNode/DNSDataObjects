ddea738d239976e4006db79edd9f5d7d9f71f59b
Darren Ehlers <me@darrenehlers.com>
2023-01-17 13:52:41 -0600

    Add NSCopying protocol to DNSPrice

--- Sources/DNSDataObjects/SimpleObjects/DNSPrice.swift
+++ Sources/DNSDataObjects/SimpleObjects/DNSPrice.swift
@@ -9,7 +9,7 @@
 import DNSCore
 import Foundation
 
-public class DNSPrice: DNSDataTranslation, Codable {
+public class DNSPrice: DNSDataTranslation, Codable, NSCopying {
     // MARK: - Properties -
     private func field(_ from: CodingKeys) -> String { return from.rawValue }
     public enum CodingKeys: String, CodingKey {
