af5af7b65d12da96f1099ffb1991c07b75b3455d
Darren Ehlers <me@darrenehlers.com>
2023-04-10 18:58:03 -0500

    Added translation methods to DNSDayOfWeekFlags

--- Sources/DNSDataObjects/SimpleObjects/DNSDayOfWeekFlags.swift
+++ Sources/DNSDataObjects/SimpleObjects/DNSDayOfWeekFlags.swift
@@ -9,7 +9,13 @@
 import DNSCore
 import Foundation
 
-open class DNSDayOfWeekFlags: Codable, Hashable, NSCopying {
+open class DNSDayOfWeekFlags: DNSDataTranslation, Codable, NSCopying {
+    // MARK: - Properties -
+    private func field(_ from: CodingKeys) -> String { return from.rawValue }
+    public enum CodingKeys: String, CodingKey {
+        case sunday, monday, tuesday, wednesday, thursday, friday, saturday
+    }
+    
     open var sunday: Bool = true
     open var monday: Bool = true
     open var tuesday: Bool = true
@@ -18,17 +24,52 @@ open class DNSDayOfWeekFlags: Codable, Hashable, NSCopying {
     open var friday: Bool = true
     open var saturday: Bool = true
     
-    public init() { }
+    // MARK: - Initializers -
+    required override public init() {
+        super.init()
+    }
+
+    // MARK: - DAO copy methods -
+    public init(from object: DNSDayOfWeekFlags) {
+        super.init()
+        self.update(from: object)
+    }
+    open func update(from object: DNSDayOfWeekFlags) {
+        self.sunday = object.sunday
+        self.monday = object.monday
+        self.tuesday = object.tuesday
+        self.wednesday = object.wednesday
+        self.thursday = object.thursday
+        self.friday = object.friday
+        self.saturday = object.saturday
+    }
     
-    // Hashable protocol methods
-    open func hash(into hasher: inout Hasher) {
-        hasher.combine(sunday)
-        hasher.combine(monday)
-        hasher.combine(tuesday)
-        hasher.combine(wednesday)
-        hasher.combine(thursday)
-        hasher.combine(friday)
-        hasher.combine(saturday)
+    // MARK: - DAO translation methods -
+    public init(from data: DNSDataDictionary) {
+        super.init()
+        _ = self.dao(from: data)
+    }
+    open func dao(from data: DNSDataDictionary) -> DNSDayOfWeekFlags {
+        self.sunday = self.bool(from: data[field(.sunday)] as Any?) ?? self.sunday
+        self.monday = self.bool(from: data[field(.monday)] as Any?) ?? self.monday
+        self.tuesday = self.bool(from: data[field(.tuesday)] as Any?) ?? self.tuesday
+        self.wednesday = self.bool(from: data[field(.wednesday)] as Any?) ?? self.wednesday
+        self.thursday = self.bool(from: data[field(.thursday)] as Any?) ?? self.thursday
+        self.friday = self.bool(from: data[field(.friday)] as Any?) ?? self.friday
+        self.saturday = self.bool(from: data[field(.saturday)] as Any?) ?? self.saturday
+        return self
+    }
+    open var asDictionary: DNSDataDictionary {
+        let retval: DNSDataDictionary = [
+            field(.sunday): self.sunday,
+            field(.monday): self.monday,
+            field(.tuesday): self.tuesday,
+            field(.wednesday): self.wednesday,
+            field(.thursday): self.thursday,
+            field(.friday): self.friday,
+            field(.saturday): self.saturday,
+        ]
+        return retval
     }
     
     // NSCopying protocol methods
